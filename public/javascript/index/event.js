3fb119f35ba389416e1a19815ffd4fca66a669da
wldlsgur <87266785+wldlsgur@users.noreply.github.com>
2022-07-12 21:10:08 +0900

    123123

--- public/javascript/index/event.js
+++ public/javascript/index/event.js
@@ -1,3 +1,19 @@
 function MovePageSignup() {
   location.href = "page/signup";
 }
+
+async function Login() {
+  let info = {
+    id: $(".login-form__id").val(),
+    pw: $(".login-form__pw").val(),
+  };
+
+  if (!info.id || !info.pw) {
+    return alert("정보를 모두 입력해주세요");
+  }
+
+  axios
+    .post("/check/login", info)
+    .then((response) => {})
+    .catch((err) => {});
+}
--- public/javascript/index/main.js
+++ public/javascript/index/main.js
@@ -1,1 +1,3 @@
 $(".login-form__singUp").click(MovePageSignup);
+$(".login-form__login").click(Login);
+// auto login
